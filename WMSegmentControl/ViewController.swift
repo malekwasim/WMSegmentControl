45cc21292fc8a3b1c34c72a031234363e5fdbe62
wasimarif.malek.hs@gmail.com <wasimarif.malek.hs@gmail.com>
2019-05-27 11:41:22 +0530

    Initial Commit

---
+++ WMSegmentControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WMSegmentControl
+//
+//  Created by Wasim Malek on 27/05/19.
+//  Copyright © 2019 Wasim Malek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
